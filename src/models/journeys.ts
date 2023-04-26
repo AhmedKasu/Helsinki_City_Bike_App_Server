74bb97016d3bf47c58175392b0c4ae92de535b24
Ahmed Kasu <ankassu@gmail.com>
2023-04-26 15:40:55 +0300

    added mongoDB compound indexes

--- src/models/journeys.ts
+++ src/models/journeys.ts
@@ -2,7 +2,8 @@ import { Schema, model } from 'mongoose';
 import { Journey } from '../types';
 
 const journeySchema = new Schema({}, { strict: false });
-
+journeySchema.index({ coveredDistanceMeters: 1, durationSeconds: 1 });
+journeySchema.index({ departureStationName: 1, returnStationName: 1 });
 const Journey = model<Journey>('Journey', journeySchema, 'journeys');
 
 export default Journey;
