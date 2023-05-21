992883af864433b9dd4d7240d724804213529dc7
Ahmed Kasu <ankassu@gmail.com>
2023-05-21 11:48:10 +0300

    added query indexes and comments

--- src/models/queryIndexes.ts
+++ src/models/queryIndexes.ts
@@ -1,11 +1,15 @@
 import { journeySchema } from './journeys';
 
+// MongoDB queries are much faster with indexes.
+//I have defined some text and compound default indexes for the most common queries.
 export const journeyIndexes = () => {
   journeySchema.index({ coveredDistanceMeters: 1, durationSeconds: 1 });
   journeySchema.index({ coveredDistanceMeters: 1, durationSeconds: -1 });
   journeySchema.index({ departureStationName: 1, returnStationName: 1 });
   journeySchema.index({ departureStationName: 1, returnStationName: -1 });
   journeySchema.index({ coveredDistanceMeters: 1, departureStationName: 1 });
+  journeySchema.index({ coveredDistanceMeters: -1 });
+  journeySchema.index({ durationSeconds: -1 });
   journeySchema.index({
     departureStationName: 1,
     returnStationName: 1,
